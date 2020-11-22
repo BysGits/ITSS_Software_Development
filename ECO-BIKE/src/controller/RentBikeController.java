cee70a4ae6ff31ecca613e7edce2733ac001912b
Hong Quan <quandh619@gmail.com>
2020-11-22 18:13:05 +0700

    Update test ECO

--- ECO-BIKE/src/controller/RentBikeController.java
+++ ECO-BIKE/src/controller/RentBikeController.java
@@ -1,5 +1,9 @@
 package controller;
 
-public class RentBikeController {
+import entity.bike.*;
 
+public class RentBikeController extends BaseController {
+	public boolean validateCardCode (String cardNumber) {
+		return false;
+	}
 }
---
+++ ECO-BIKE/test/ValidateCardCodeTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controller.RentBikeController;
+
+class ValidateCardCodeTest {
+
+	private RentBikeController rentBikeController;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		rentBikeController = new RentBikeController();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	void validateCardCodeTest 
+
+}
