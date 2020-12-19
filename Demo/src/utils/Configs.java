5613b904cc159f1de882a6665e1c77350888c36c
I001user <hung.nt176773@sis.hust.edu.vn>
2020-12-19 17:13:13 +0700

    saving my work

--- Demo/src/utils/Configs.java
+++ Demo/src/utils/Configs.java
@@ -4,12 +4,12 @@ public class Configs {
 
 	
 	// static resource
-	public static final String HOME_PATH = "/views/fxml/home.fxml";
+	public static final String HOME_PATH = "/views/fxml/home_1.fxml";
 	public static final String SPLASH_SCREEN_PATH = "/views/fxml/splash.fxml";
 	public static final String RENT_BIKE_PATH = "/views/fxml/rent_bike.fxml";
 	public static final String VIEW_BIKE_PATH = "/views/fxml/view_bike.fxml";
 	public static final String CARD_PATH = "/views/fxml/card.fxml";
-	public static final String DOCK_HOME_PATH = "/views/fxml/dock_home.fxml";
+	public static final String DOCK_HOME_PATH = "/views/fxml/dock_home_2.fxml";
 	public static final String DOCK_PATH = "/views/fxml/bike.fxml";
 	public static final String DOCK_BIKE_PATH = "/views/fxml/dock_bike.fxml";
 	public static final String BIKE_INFO_PATH = "/views/fxml/bike_info.fxml";
